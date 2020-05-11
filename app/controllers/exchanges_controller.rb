ef9f53d9f11214d3239766f50127b7beba3854c4
nailson <nailsongomes@gmail.com>
2020-05-11 16:23:40 -0300

    First commit

---
+++ app/controllers/exchanges_controller.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class ExchangesController < ApplicationController
+  def index; end
+
+  def convert
+    value = ExchangeService.new(
+      params[:source_currency],
+      params[:target_currency],
+      params[:amount]
+    ).call
+    render json: { "value": value }
+  end
+end
---
+++ spec/services/exchange_service_spec.rb
@@ -0,0 +1,45 @@
+require 'rails_helper'
+require 'json'
+require './app/services/exchange_service'
+
+describe ExchangeService do
+  let(:source_currency) { 'USD' }
+  let(:target_currency) { 'BRL' }
+  let(:exchange_value) { 3.4546 }
+  let(:api_return) do
+    {
+      currency: [
+        {
+           currency: '#{source_currency}/#{target_currency}',
+           value: exchange_value,
+           date: Time.now,
+           type: 'Original'
+        }
+      ]
+    }
+  end
+
+  before do
+    allow(RestClient).to receive(:get) { 
+      OpenStruct.new(
+        body: api_return.to_json
+      ) 
+    }
+  end
+
+  it '#call' do
+    # setup
+    amount = rand(0..9999)
+
+    # exercise
+    service_exchange = ExchangeService.new(
+      'USD',
+      'BRL',
+      amount
+    ).call
+    expected_exchange = amount * exchange_value
+
+    # verify
+    expect(service_exchange).to eq(expected_exchange)
+  end
+end
